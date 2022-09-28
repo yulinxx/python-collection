b5394691d85d8e15ed97ce804a25bdd8e2ec1937
yulinxx <yulinxx@gmail.com>
2022-09-28 21:24:10 +0800

    添加二进制加密文件的复制,待測試

--- xFlie.py
+++ xFlie.py
@@ -2,12 +2,13 @@ import os
 from shutil import copyfile
 
 class HandleFile:
-    def __init__(self, originDir, typeList, newDir, ignoreDir, addSuffix):
-        self.originDir = originDir
-        self.typeList = typeList
-        self.newDir = newDir
-        self.ignoreDir = ignoreDir
-        self.addSuffix = addSuffix
+    def __init__(self, origin_dir, txt_list, new_dir, ignore_dir, add_suffix, bin_list=[]):
+        self.__originDir = origin_dir
+        self.__txtList = txt_list
+        self.__binList = bin_list
+        self.__newDir = new_dir
+        self.__ignoreDir = ignore_dir
+        self.__addSuffix = add_suffix
 
     def copyFiles(self):
         """
@@ -18,29 +19,45 @@ class HandleFile:
             Returns:  共处理的文件数量\n
         """
         # 在这里,将老目录去掉
-        pathLen = len(self.originDir)
+        pathLen = len(self.__originDir)
         countIndex = 0
 
-        for root, dirname, filename in os.walk(originDir):
-            for curDir in dirname:
-                if curDir in self.ignoreDir:
+        for rootDir, dirName, fileNames in os.walk(originDir):
+
+            for curDir in dirName:
+                if curDir in self.__ignoreDir:  # 忽略的文件夹不复制
                     continue
                 else:
                     break
 
-            mkDir = self.newDir + root[pathLen:]
-
             # 若目录不存在,则创建新的目录进行文件存储
+            mkDir = self.__newDir + rootDir[pathLen:]
             if not os.path.exists(mkDir):
                 os.mkdir(mkDir)
 
-            for strName in filename:
+            for strFileName in fileNames:
                 countIndex += 1
 
-                originFile = os.path.join(root, strName)
+                originFile = os.path.join(rootDir, strFileName)
                 fInfo = os.path.splitext(originFile)
-                if fInfo[-1].lower() in typeList:
-                    newFileName = mkDir + '/' + strName + self.addSuffix
+
+                if fInfo[-1].lower() in self.__binList:  # 二进制文件的读写复制
+                    try:
+                        binOpen = open(originFile, 'rb')
+                        binCont = binOpen.read()
+                        binOpen.close()
+
+                        newFileName = mkDir + '/' + strFileName + self.__addSuffix
+                        newFile = open(newFileName, 'wb')
+                        e = newFile.write(binCont)
+                        newFile.close()
+                    except:
+                        print(f"---- Error {originFile} to {newFileName}")
+                    finally:
+                        print(f"{originFile} ---> {newFileName}")
+
+                elif fInfo[-1].lower() in self.__txtList:  # 符合的后缀文件进行后缀改写复制
+                    newFileName = mkDir + '/' + strFileName + self.__addSuffix
                     # 新路径 + 源文件名称 + 新增后缀
                     with open(originFile, 'r', encoding='utf-8') as f:
                         with open(newFileName, 'w', encoding='utf-8') as newFile:
@@ -51,8 +68,9 @@ class HandleFile:
                             finally:
                                 print(f"{newFileName} ---> {countIndex}")
                 else:  # 若不存在列表中(未加密的文件),则直接进行复制
-                    newFileName = mkDir + '/' + strName
+                    newFileName = mkDir + '/' + strFileName
                     copyfile(originFile, newFileName)
+                    print(f"{newFileName} ---> {countIndex}")
 
         print('copyFile Process: ' + str(countIndex) + ' files')
 
@@ -63,17 +81,17 @@ class HandleFile:
             param1 - 要处理的文件夹     param2 - 要去掉的后缀名\n
             Returns:  共处理的文件数量\n
         """
-        removeLen = len(self.addSuffix)
+        removeLen = len(self.__addSuffix)
         countIndex = 0
 
-        for root, dirname, filename in os.walk(self.newDir):
+        for root, dirname, filename in os.walk(self.__newDir):
             for f in filename:
                 # 取得文件名的后面的字符(数量和增加的后缀一致)
                 strSuffix = f[-removeLen:]
 
                 # 判断文件后面的字符 是否是 新增的后缀
                 # 若是新增的后缀,则去掉, 否则,不作处理
-                if strSuffix == self.addSuffix:
+                if strSuffix == self.__addSuffix:
                     fileName = os.path.join(root, f)
                     newName = fileName[:-removeLen]
                     if not os.path.exists(newName):
@@ -90,18 +108,25 @@ if __name__ == '__main__':
     print('-------------------------')
     print('----------Begin----------')
 
-    originDir = r'E:/RD/GraphicsComponent_QtExamplexx20220701'  # 源路径
-    newDir = r'D:/xx/Downloads/LANDrop/GraphicsComponent_QtExamplexxx'  # 新路径
+    optCopy = True
+    originDir = r'E:/pyProj/python-collection/.git'  # 源路径
+
+    newDir = r'E:/pyProj/python-collection/gitxxx'  # 新路径 或 要还原的路径
+
     ignoreDir = ['.vs', '.vscode', '.ide', 'build']
     addSuffix = r'_xx'  # 添加后缀
-    typeList = ['.h', '.cpp', '.txt', '.hpp', '.inl', '.py', '.c', '.cxx',
-                '.cmake', '.idx', '.rc', '.ui', '.qrc', '.pro', '.pri']  # 文件类型
+    txtFileList = ['.h', '.cpp', '.txt', '.hpp', '.inl', '.py', '.c', '.cxx',
+                   '.cmake', '.rc', '.ui', '.qrc', '.pro', '.pri']  # 文件类型
 
-    Handle = HandleFile(originDir, typeList, newDir, ignoreDir, addSuffix)
-    print('......Step 1:')
-    # Handle.copyFiles()
+    binList = ['.idx']
 
-    print('......Step 2:')
-    Handle.rnameSuffix()
+    HandleObj = HandleFile(originDir, txtFileList, newDir, ignoreDir, addSuffix, binList)
+
+    if optCopy:
+        print('......复制文件:')
+        HandleObj.copyFiles()
+    else:
+        print('......还原文件:')
+        HandleObj.rnameSuffix()
 
     print('......The End......')
