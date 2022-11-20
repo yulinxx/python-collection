83da9d4fecf7c4b47919316caa31cd1a1e2d0bad
yulinxx <yulinxx@gmail.com>
2022-11-20 21:19:05 +0800

    test

--- SM_Job/jobManager.py
+++ SM_Job/jobManager.py
@@ -1,94 +1,104 @@
 class jobManager:
-    def __init__(self, userList, monthData):
-        self.__userList = userList  # 人员列表
-        self.__monthData = monthData  # 月历数据
+    def __init__(self, userList, monthData, schedule_days):
+        self._userList = userList  # 人员列表
+        self._monthData = monthData  # 月历数据
 
         self._curPersonIndex = 0
         self._curPersonDays = 0
 
-        self._ScheduleDays = 7
+        self._ScheduleDays = schedule_days
 
         # k/v  人员/安排的时间
         self._userDataDict = {}
         for user in userList:
             self._userDataDict[user] = []
 
+
     def SetUser(self, userList):
-        self.__userList = userList
+        """设置参与排班的用户列表"""
+        self._userList = userList
         self._userDataDict = {user: [] for user in userList}
+
     def SetMonthData(self, monthData):
-        self.__monthData = monthData
-        self._userDataDict = {user: [] for user in self.__userList}
+        """设置这个月需要排班的日期"""
+        self._monthData = monthData
+        self._userDataDict = {user: [] for user in self._userList}
+        self._dateDict = {}
 
-    # def __str__(self):
-    def ShowData(self):
-        """print"""
-        for key in self._userDataDict:
-            print(key)
+    def get_schedule(self):
+        """获取排了班的数据"""
+        date_schedule = {}
+        for user_data in self._userDataDict.items():
+            for i in user_data[1]:
+                date_schedule[i] = user_data[0]
+        return date_schedule
 
-            v = self._userDataDict[key]
-            print(v)
-            # for l in v:
-            #     print(l)
-
-    def GetCurrentPersonIndex(self):
+    def getCurPersonIndex(self):
         """查询分配的人员索引"""
         return self._curPersonIndex
+    
+    def getCurPersonDays(self):
+        """查询分配的人员分配了多少天"""
+        return self._curPersonDays
 
-    def Do(self, indexPerson: int = 0):
+    def setCurPersonIndex(self, index):
+        """设置分配的人员索引"""
+        self._curPersonIndex = index
+
+    def setCurPersonDays(self, day):
+        """查询分配的人员分配了多少天"""
+        self._curPersonDays = day
+
+    def Do(self, indexPerson: int=0):
         """设置排班日期"""
 
         curDayIndex = 0  # 当前分配的日期索引
 
         # 第一次分配,特殊处理
         # 判断上次是否有人员分配未完整
-        curUser = self.__userList[indexPerson]  # 当前分配的人员
+        curUser = self._userList[indexPerson]  # 当前分配的人员
         if self._curPersonDays > 0:  # 上次有人只分配了 N 天
             # 该人还需分配的天数6
             needDays = self._ScheduleDays - self._curPersonDays
 
-            countDays = len(self.__monthData)
-            if countDays > needDays:    # 若该月的天数够用
-                temp = self.__monthData[0:needDays]
-                self._userDataDict[curUser].extend(self.__monthData[0:needDays])  # 补上个月剩余的天数
+            countDays = len(self._monthData)
+            if countDays > needDays:  # 若该月的天数够用
+                temp = self._monthData[0:needDays]
+                self._userDataDict[curUser].extend(self._monthData[0:needDays])  # 补上个月剩余的天数
                 curDayIndex += needDays
                 indexPerson += 1
-            else:   # 不够用
-                self._userDataDict[curUser].extend(self.__monthData[0:])  # 补上个月剩余的天数
+            else:  # 不够用
+                self._userDataDict[curUser].extend(self._monthData[0:])  # 补上个月剩余的天数
                 self._curPersonIndex = indexPerson
-                self._curPersonDays += countDays # 再追加该月的天数
+                self._curPersonDays += countDays  # 再追加该月的天数
                 return
 
         loopUser = True
         while loopUser:  # 遍历用户
-            for user in self.__userList[indexPerson:]:
-                if len(self.__monthData[curDayIndex:]) >= self._ScheduleDays:
-                    self._userDataDict[user].extend(self.__monthData[curDayIndex:curDayIndex + self._ScheduleDays])
+            for user in self._userList[indexPerson:]:
+                if len(self._monthData[curDayIndex:]) >= self._ScheduleDays:
+                    self._userDataDict[user].extend(self._monthData[curDayIndex:curDayIndex + self._ScheduleDays])
                     curDayIndex += self._ScheduleDays
 
                     indexPerson += 1
 
-                    # indexPerson %= len(self.__userList)
+                    # indexPerson %= len(self._userList)
 
-                    if indexPerson > len(self.__userList) - 1:
+                    if indexPerson > len(self._userList) - 1:
                         indexPerson = 0
 
-                    self._curPersonDays = 0   # 还需要分配的天数
+                    self._curPersonDays = 0  # 还需要分配的天数
 
-                else:   # 剩余所有的月份分配给指定人员
-                    self._userDataDict[user].extend(self.__monthData[curDayIndex:])
+                else:  # 剩余所有的月份分配给指定人员
+                    self._userDataDict[user].extend(self._monthData[curDayIndex:])
 
-                    self._curPersonDays = len(self.__monthData[curDayIndex:])  # 当前只分配了N天
+                    self._curPersonDays = len(self._monthData[curDayIndex:])  # 当前只分配了N天
                     self._curPersonIndex = indexPerson  # 保存当前分配的人员索引
-
-                    # m = 3
-                    # x = 9-9
-                    # n = m / x
 
                     loopUser = False
                     break
 
-        print("End of While")
+        return self.get_schedule()
 
     ####################################################
     def SetScheduleDays(self, days: int = 7):
@@ -97,18 +107,14 @@ class jobManager:
     def GetScheduleDays(self):
         return self._ScheduleDays
 
-    def SetUser(self, userList):
-        """设置排班人员"""
-        self.__userList = userList
-
     def AddUser(self, user):
         """添加单个排班人员"""
-        # self.__userList.append(user)
+        # self._userList.append(user)
         pass
 
     def DelUser(self, user):
         """删除排班人员"""
-        # self.__userList.delte(user)
+        # self._userList.delete(user)
         pass
 
     def SortUserUp(self, user):
