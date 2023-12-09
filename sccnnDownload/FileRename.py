ad0b30375413222427a0f389b9965ce3d210625e
yulinxx <yulinxx@gmail.com>
2023-12-10 00:56:14 +0800

    新增 素材下载并解压后，将文件移至最上级目录，并重命名2

--- sccnnDownload/FileRename.py
+++ sccnnDownload/FileRename.py
@@ -12,6 +12,31 @@ def is_small_file(file_path, size_limit_kb=2):
     return os.path.getsize(file_path) < size_limit_kb * 1024
 
 
+def move_files(from_folder, to_folder, file_name):
+    # 复制子文件夹的文件到目标路径
+    for root, dirs, files in os.walk(from_folder):
+        for file in files:
+            source_path = os.path.join(root, file)
+
+            # 构建目标文件路径为第一级子文件夹的上一级目录，并在文件名前添加第一级子文件夹的名称
+            target_path = os.path.join(to_folder, f"{file_name}_{file}")
+
+            # 判断文件是否是快捷方式或大小小于2K
+            if is_shortcut(source_path) or is_small_file(source_path):
+                try:
+                    os.remove(source_path)  # 尝试删除文件
+                    print(f"Deleted: {source_path}")
+                except Exception as e:
+                    print(f"Error deleting {source_path}: {e}")
+
+                print(f"Deleted: {source_path}")
+            else:
+                try:
+                    shutil.move(source_path, target_path)  # 使用shutil.move移动文件
+                    print(f"Moved: {source_path} to {target_path}")
+                except Exception as e:
+                    print(f"Error moving {source_path} to {target_path}: {e}")
+
 def move_and_rename_files(main_folder):
     # 获取主文件夹下的所有子文件夹
     sub_folders = [f.path for f in os.scandir(main_folder) if f.is_dir()]
@@ -21,6 +46,14 @@ def move_and_rename_files(main_folder):
         # 获取用于命名的文件夹的名称，即第一级目录的文件夹名称
         folder_name = os.path.basename(sub_a_folder)
 
+        # ***************************************
+
+        # ***************************************
+        # 构建目标路径为第一级子文件夹的上一级目录
+        target_folder = os.path.dirname(sub_a_folder)
+
+        move_files(sub_a_folder, target_folder, folder_name)
+
         # 获取子文件夹下的所有子文件夹
         sub_b_folders = [f.path for f in os.scandir(sub_a_folder) if f.is_dir()]
 
@@ -28,32 +61,8 @@ def move_and_rename_files(main_folder):
             # 获取最终子文件夹的名称
             final_folder_name = os.path.basename(last_folder)
 
-            # 构建目标路径为第一级子文件夹的上一级目录
-            target_folder = os.path.dirname(sub_a_folder)
-
             # 复制子文件夹的文件到目标路径
-            for root, dirs, files in os.walk(last_folder):
-                for file in files:
-                    source_path = os.path.join(root, file)
-
-                    # 构建目标文件路径为第一级子文件夹的上一级目录，并在文件名前添加第一级子文件夹的名称
-                    target_path = os.path.join(target_folder, f"{folder_name}_{file}")
-
-                    # 判断文件是否是快捷方式或大小小于2K
-                    if is_shortcut(source_path) or is_small_file(source_path):
-                        try:
-                            os.remove(source_path)  # 尝试删除文件
-                            print(f"Deleted: {source_path}")
-                        except Exception as e:
-                            print(f"Error deleting {source_path}: {e}")
-
-                        print(f"Deleted: {source_path}")
-                    else:
-                        try:
-                            shutil.move(source_path, target_path)  # 使用shutil.move移动文件
-                            print(f"Moved: {source_path} to {target_path}")
-                        except Exception as e:
-                            print(f"Error moving {source_path} to {target_path}: {e}")
+            move_files(last_folder, target_folder, folder_name)
 
 
 # 删除空文件夹
@@ -71,6 +80,6 @@ def delete_empty_folders(folder_path):
 
 
 if __name__ == "__main__":
-    main_folder = "D:/xx/Pictures/Matter/M/ttt"
+    main_folder = "D:/xx/Pictures/Matter/M"
     move_and_rename_files(main_folder)
     delete_empty_folders(main_folder)
