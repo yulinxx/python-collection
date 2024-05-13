407e4ba9748123a8a8e807b9668231502a9e96df
Don't follow me <yulinxx@gmail.com>
2024-05-13 19:42:42 +0800

    调整 -- 根据文本文件,决定复制

--- xFlie.py
+++ xFlie.py
@@ -3,35 +3,42 @@ from shutil import copyfile
 
 
 class HandleFile:
-    def __init__(self, origin_dir, txt_list, new_dir, ignore_dir, add_suffix, bin_list=[]):
-        self.__originDir = origin_dir
-        self.__txtList = txt_list
-        self.__binList = bin_list
-        self.__newDir = new_dir
-        self.__ignoreDir = ignore_dir
-        self.__addSuffix = add_suffix
+    def __init__(self, origin_dir, new_dir, ignore_dir):
+        self.origin_dir = origin_dir
+        self.new_dir = new_dir
+        self.ignore_dir = ignore_dir
+        self.add_suffix = '_aprilxx'
 
-    def copyFiles(self):
+    def __is_txt_file(self, fileName:str):
+        #return True
+        try:
+            with open(fileName, 'r', encoding='utf-8') as file:
+                for line in file:
+                    break
+            return True
+        except UnicodeDecodeError:
+            #print(f'\n!!!!!!!!!!!! {fileName} xError UnicodeDecodeError \n')
+            return False
+        except Exception as e:
+            #print(f'\n!!!!!!!!!!!! {fileName} xError: {e}\n')
+            return False
+
+    def copy_files(self):
         """
-            这个是函数的帮助说明文档，help时会显示\n
-            将文件夹中的文件复制至新文件中,并添加后缀名,只处理列表中的文件类型\n
-            param1 - 要处理的文件夹  param2 -  要处理的文件类型\n
-            param3 - 存储的文件夹    param4 - 新文件要添加的后缀名\n
-            Returns:  共处理的文件数量\n
         """
-        # 在这里,将老目录去掉strFind = {str} '\\'
+        # 在这里,将老目录去掉 strFind = {str} '\\'
         countIndex = 0
         parentPath = '@'
-        pathLen = len(self.__originDir)
+        pathLen = len(self.origin_dir)
         errorList = []
 
         # 当前文件夹的路径 子目录列表  当前路径下的所有文件
-        for rootDir, dirName, fileNames in os.walk(originDir):
+        for rootDir, dirName, fileNames in os.walk(self.origin_dir):
             strFind = '\\'
             rootDir = rootDir.replace('\\', '/')
             nPos = rootDir.rfind('/')
             curDir = rootDir[nPos + len(strFind):]  # 当前文件夹路径中的最后的文件夹名称
-            if curDir in self.__ignoreDir:  # 忽略的文件夹不复制
+            if curDir in self.ignore_dir:  # 忽略的文件夹不复制
                 parentPath = rootDir  # 保存当前路径,防止子目录继续遍历
                 continue
 
@@ -42,14 +49,13 @@ class HandleFile:
             parentPath = '@'
 
             # 若目录不存在,则创建新的目录进行文件存储
-            # mkDir = rootDir.replace(self.__originDir, self.__newDir)
-            mkDir = self.__newDir + '/' + rootDir[pathLen:]
+            # mkDir = rootDir.replace(self.origin_dir, self.new_dir)
+            mkDir = self.new_dir + '/' + rootDir[pathLen:]
             if not os.path.exists(mkDir):
                 try:
                     os.mkdir(mkDir)
                 except:
                     continue
-                    pass
 
             for strFileName in fileNames:  # 遍历当前文件夹下所有的文件
                 countIndex += 1
@@ -58,60 +64,57 @@ class HandleFile:
                 fInfo = os.path.splitext(originFile)
 
                 fileSuffix = fInfo[-1].lower()
-                if fileSuffix in self.__binList:  # 二进制文件的读写复制
+
+                if self.__is_txt_file(originFile)  or fileSuffix.lower() == ".idx":
                     try:
                         binOpen = open(originFile, 'rb')
                         binCont = binOpen.read()
                         binOpen.close()
 
-                        newFileName = mkDir + '/' + strFileName + self.__addSuffix
+                        newFileName = mkDir + '/' + strFileName + self.add_suffix
                         newFile = open(newFileName, 'wb')
                         e = newFile.write(binCont)
                         newFile.close()
                     except:
-                        print(f"\nxxxError {fileSuffix} -> {originFile} to {newFileName}\n")
-                        errorList.append(f"\nxxxError {fileSuffix} -> {originFile} to {newFileName}\n")
+                        print(f"\n!!!!!!!!!!!! xError {fileSuffix} -> {originFile} to {newFileName}\n")
+                        errorList.append(f"\n!!!!!!!!!!!! xxx xError {fileSuffix} -> {originFile} to {newFileName}\n")
                     finally:
+                        if len(fileSuffix) < 1:
+                            fileSuffix = "        "
+
                         print(f"{fileSuffix} -- {originFile} ---> {newFileName}")
-
-                elif fileSuffix in self.__txtList:  # 符合的后缀文件进行后缀改写复制
-                    newFileName = mkDir + '/' + strFileName + self.__addSuffix
-                    # 新路径 + 源文件名称 + 新增后缀
-                    with open(originFile, 'r', encoding='utf-8') as f:
-                        with open(newFileName, 'w', encoding='utf-8') as newFile:
-                            try:
-                                newFile.write(f.read())
-                            except:
-                                print(f"\nxxxError {newFileName} -> {originFile}\n")
-                            finally:
-                                print(f"{fileSuffix} -- {newFileName} ---> {countIndex}")
-                else:  # 若不存在列表中(未加密的文件),则直接进行复制
+                        
+                else:  # 不是文本文件,则直接进行复制
                     newFileName = mkDir + '/' + strFileName
-                    copyfile(originFile, newFileName)
-                    print(f"{fileSuffix} -- {newFileName} ---> {countIndex}")
+                    dst = copyfile(originFile, newFileName)
+                    if dst:
+                        print(f"{fileSuffix} -- copy: {newFileName} ---> {countIndex}")
+                    else:
+                        errorList.append(f"\n!!!!!!!!!!!! Copy xError {fileSuffix} -> {originFile} to {newFileName}\n")
+                        print(f"{fileSuffix} -- copy: {newFileName} ---> {countIndex}")
 
-        print('copyFile Process: ' + str(countIndex) + ' files')
+        print('copyFile: ' + str(countIndex) + ' files')
         for e in errorList:
             print(e)
-
+            
     def rnameSuffix(self):
 
         """
             将文件夹中的文件名去掉指定的后缀\n
-            param1 - 要处理的文件夹     param2 - 要去掉的后缀名\n
-            Returns:  共处理的文件数量\n
         """
-        removeLen = len(self.__addSuffix)
+        removeLen = len(self.add_suffix)
         countIndex = 0
 
-        for root, dirname, filename in os.walk(self.__newDir):
+        errorList = []
+
+        for root, dirname, filename in os.walk(self.new_dir):
             for f in filename:
                 # 取得文件名的后面的字符(数量和增加的后缀一致)
                 strSuffix = f[-removeLen:]
 
                 # 判断文件后面的字符 是否是 新增的后缀
                 # 若是新增的后缀,则去掉, 否则,不作处理
-                if strSuffix == self.__addSuffix:
+                if strSuffix == self.add_suffix:
                     fileName = os.path.join(root, f)
                     newName = fileName[:-removeLen]
                     if not os.path.exists(newName):
@@ -119,40 +122,39 @@ class HandleFile:
                     else:
                         os.remove(fileName)
 
+                    print(f"{fileName} -- copy: {newName} ---> {countIndex}")
+
                 countIndex += 1
-                print('rnameSuffix Process: ' + str(countIndex) + ' files')
+                
 
 
 if __name__ == '__main__':
     print('-------------------------')
     print('----------Begin----------')
 
-    # optCopy = True
-    optCopy = False
-    originDir = r'C:/CAD/xxx/'  # 源路径
+    origin_dir = r'C:/CAD/MantiSoft/'  # 源路径
+    new_dir = r'E:/MantiSoft'  # 新路径 或 要还原的路径
 
-    newDir = r'E:/RD/CAD20221230'  # 新路径 或 要还原的路径
+    ignore_dir = ['.vs', '.vscode', '.ide', 'build', 'CMake-build']
 
-    ignoreDir = ['.vs', '.vscode', '.ide', 'build', 'CMake-build', 'x64']
-    binList = ['.idx']  # 以二进制复制的文件后缀
+    handle_obj = HandleFile(origin_dir, new_dir, ignore_dir)
 
-    addSuffix = r'_xx'  # 添加后缀
-    txtFileList = ['.h', '.cpp', '.txt', '.hpp', '.inl', '.py', '.c', '.cxx',
-                   '.cmake', '.rc', '.ui', '.qrc', '.pro', '.pri', '.sln']  # 文件类型
-
-    HandleObj = HandleFile(originDir, txtFileList, newDir, ignoreDir, addSuffix, binList)
-
-    if optCopy:
-        if not os.path.exists(originDir):
+    opt_copy = True
+    #opt_copy = False
+    
+    if opt_copy:
+        if not os.path.exists(origin_dir):
             print('......路径有问题:')
             raise OSError
+
         print('......开始复制文件:')
-        HandleObj.copyFiles()
+        handle_obj.copy_files()
     else:
-        if not os.path.exists(newDir):
+        if not os.path.exists(new_dir):
             print('......路径有问题:')
             raise OSError
+            
         print('......开始还原文件:')
-        HandleObj.rnameSuffix()
-
-    print('......The End......')
+        handle_obj.rnameSuffix()
+        
+    print('\n......The End......')
